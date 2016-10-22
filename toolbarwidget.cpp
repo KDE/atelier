921195ae08dcf1f44ad741ca6bbabd34c9801035
Lays Rodrigues <laysrodriguessilva@gmail.com>
2016-10-22 20:34:59 -0200

    New Files  - Main Window  - ToolBar Widget to handle some basic actions
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

---
+++ toolbarwidget.cpp
@@ -0,0 +1,21 @@
+#include "toolbarwidget.h"
+#include <QHBoxLayout>
+
+ToolBarWidget::ToolBarWidget(QWidget *parent) : QWidget(parent),
+    settingsTB(new QToolButton),
+    openFileTB(new QToolButton),
+    connectTB(new QToolButton),
+    startTB(new QToolButton),
+    pauseTB(new QToolButton),
+    stopTB(new QToolButton)
+{
+    auto *layout = new QHBoxLayout;
+    layout->addWidget(settingsTB);
+    layout->addWidget(openFileTB);
+    layout->addWidget(connectTB);
+    layout->addSpacing(400);
+    layout->addWidget(startTB);
+    layout->addWidget(pauseTB);
+    layout->addWidget(stopTB);
+    this->setLayout(layout);
+}
---
+++ toolbarwidget.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <QWidget>
+#include <QToolButton>
+class ToolBarWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ToolBarWidget(QWidget *parent = nullptr);
+
+private:
+    QToolButton *settingsTB;
+    QToolButton *openFileTB;
+    QToolButton *connectTB;
+    QToolButton *startTB;
+    QToolButton *pauseTB;
+    QToolButton *stopTB;
+};
