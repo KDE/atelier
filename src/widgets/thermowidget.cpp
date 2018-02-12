7001e58ee078728d47b08ba96af307ccd3048299
Tomaz Canabrava <tcanabrava@kde.org>
2018-02-12 01:41:11 +0100

    Implemented Page Up / Down handling

--- src/widgets/thermowidget.cpp
+++ src/widgets/thermowidget.cpp
@@ -31,15 +31,29 @@ void ThermoWidget::keyPressEvent(QKeyEvent* event)
         m_targetTemperature = m_currentTemperatureTextFromEditor.toInt();
     } else if (event->key() == Qt::Key_Escape) {
         m_currentTemperatureTextFromEditor = '0';
-    } else if (ev->key() == Qt::Key_Up || ev->key() == Qt::Key_Right) {
+    } else if (event->key() == Qt::Key_Up || event->key() == Qt::Key_Right) {
         if (m_targetTemperature != upperBound()) {
             m_currentTemperatureTextFromEditor = QString::number(m_targetTemperature + 1);
         }
-    } else if (ev->key() == Qt::Key_Down || ev->key() == Qt::Key_Left) {
+    } else if (event->key() == Qt::Key_Down || event->key() == Qt::Key_Left) {
         if (m_targetTemperature != lowerBound()) {
             m_currentTemperatureTextFromEditor = QString::number(m_targetTemperature - 1);
         }
-    } else {
+    } else if (event->key() == Qt::Key_PageUp) {
+        if (m_targetTemperature + 10 > upperBound()) {
+            m_currentTemperatureTextFromEditor = QString::number(upperBound());
+        } else {
+            m_currentTemperatureTextFromEditor = QString::number(m_targetTemperature + 10);
+        }
+    } else if (event->key() == Qt::Key_PageDown) {
+        if (m_targetTemperature - 10 < lowerBound()) {
+            m_currentTemperatureTextFromEditor = QString::number(lowerBound());
+        } else {
+            m_currentTemperatureTextFromEditor = QString::number(m_targetTemperature - 10);
+        }
+    }
+
+    else {
         QwtDial::keyPressEvent(event);
         return;
     }
