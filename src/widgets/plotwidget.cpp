cc32375756651fcd47a7c918e52723a3f1beee3e
Chris Rizzitello <rizzitello@kde.org>
2018-03-28 16:00:30 -0400

    Sync to atcore widget
    Signed-off-by: Chris Rizzitello <rizzitello@kde.org>

--- src/widgets/plotwidget.cpp
+++ src/widgets/plotwidget.cpp
@@ -2,10 +2,13 @@
     Copyright (C) <2016>
     Author: Patrick José Pereira - patrickelectric@gmail.com
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 3 of
+    the License or any later version accepted by the membership of
+    KDE e.V. (or its successor approved by the membership of KDE
+    e.V.), which shall act as a proxy defined in Section 14 of
+    version 3 of the license.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,23 +19,26 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "plotwidget.h"
 #include <QChart>
-#include <KLocalizedString>
+
+#include "plotwidget.h"
 
 PlotWidget::PlotWidget(QWidget *parent) :
     QWidget(parent)
 {
     _chart = new QChartView;
+    if (palette().text().color().value() >= QColor(Qt::lightGray).value()) {
+        _chart->chart()->setTheme(QChart::ChartThemeDark);
+    }
     QHBoxLayout *mainLayout = new QHBoxLayout;
     mainLayout->addWidget(_chart);
     this->setLayout(mainLayout);
 
-    newPlot(i18n("Actual Bed"));
-    newPlot(i18n("Target Bed"));
+    newPlot(tr("Actual Bed"));
+    newPlot(tr("Target Bed"));
     // The extruder need to be added after some signal emitted (ExtruderCountChanged)
-    newPlot(i18n("Actual Ext.1"));
-    newPlot(i18n("Target Ext.1"));
+    newPlot(tr("Actual Ext.1"));
+    newPlot(tr("Target Ext.1"));
     update();
 }
 
@@ -74,7 +80,7 @@ void PlotWidget::update()
     axisX->setFormat(QStringLiteral("hh:mm:ss"));
 
     axisY->setLabelFormat(QStringLiteral("%d"));
-    axisY->setTitleText(i18n("Temp."));
+    axisY->setTitleText(tr("Temp."));
 
     _chart->chart()->addAxis(axisY, Qt::AlignLeft);
     _chart->chart()->addAxis(axisX, Qt::AlignBottom);
