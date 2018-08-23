46f0465c0be53f5f109f3064e18e48eacc634f3d
Kevin Ottens <ervin@kde.org>
2018-08-23 23:40:37 +0200

    Rework the GridMesh generation
    We now output 40 lines as expected instead of 800 lines overlapping 20
    times.
    
    Differential Revision: https://phabricator.kde.org/D15042

--- src/widgets/3dview/gridmesh.cpp
+++ src/widgets/3dview/gridmesh.cpp
@@ -21,8 +21,8 @@
 */
 
 #include <QGeometryRenderer>
+#include <QSize>
 #include <QVector3D>
-#include <QVector2D>
 #include "gridmesh.h"
 #include "linemeshgeometry.h"
 
@@ -33,15 +33,17 @@ GridMesh::GridMesh(Qt3DCore::QNode *parent) : Qt3DRender::QGeometryRenderer(pare
     setFirstInstance(0);
     setPrimitiveType(Qt3DRender::QGeometryRenderer::Lines);
 
-    QVector2D s(20, 20);
+    QSize meshResolution(20, 20);
     QVector<QVector3D> vertices;
-    for (uint i = 0; i <= s.x(); i++) {
-        for (uint j = 0; j <= s.y(); j++) {
-            vertices.append(QVector3D(i, 0, 0));
-            vertices.append(QVector3D(i, j, 0));
-            vertices.append(QVector3D(0, j, 0));
-            vertices.append(QVector3D(i, j, 0));
-        }
+
+    for (int x = 0; x <= meshResolution.width(); x++) {
+        vertices.append(QVector3D(x, 0, 0));
+        vertices.append(QVector3D(x, meshResolution.width(), 0));
+    }
+
+    for (int y = 0; y <= meshResolution.height(); y++) {
+        vertices.append(QVector3D(0, y, 0));
+        vertices.append(QVector3D(meshResolution.height(), y, 0));
     }
 
     auto geometry = new LineMeshGeometry(vertices, this);
